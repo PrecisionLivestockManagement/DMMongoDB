b08052ea1317fff6f65cdddfe55ce7858009b62b
DavoSwain <d.swain@cqu.edu.au>
2019-02-06 10:41:43 +1000

    Made changes to the weekly and daily functions to be able to pass a list of RFID tags and return list of tags and the station they are from plus a dataframe with weights and dates for either weekly or daily data and be able to restrict to time periods.

--- R/dailywts.R
+++ R/dailywts.R
@@ -3,33 +3,42 @@
 #' @export
 
 
-dailywts <- function(RFID, start, end){
+dailywts <- function(RFID, start=NULL, end=NULL){
 
 
   username = keyring::key_list("DMMongoDB")[1,2]
   password =  keyring::key_get("DMMongoDB")
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
-  cattle <- mongo(collection = "Cattle", db = "DataMuster",
-    url = pass,
-    verbose = T)
+
+  cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
 
 
-  filterstation <- sprintf('{"RFID":"%s"}', RFID)
+  RFID <- paste(unlist(RFID), collapse = '", "' )
+  filterstation <- sprintf('{"RFID":{"$in":["%s"]}}', RFID)
   jan <- cattle$find(query = filterstation, fields='{"RFID":true, "stationname":true, "wthist.date":true, "wthist.weight":true, "_id":false}')
 
-  dailywts <- setNames(data.frame(matrix(ncol = 2, nrow = length(jan$wthist$date[[1]]))), c("Date", "Weight"))
+  cattleinfo <- list(RFID=jan$RFID, Station=jan$stationname)
 
-  dailywts$Date <- jan$wthist$date[[1]]
-  dailywts$Weight <- jan$wthist$weight[[1]]
+  for(i in 1:length(jan$RFID)){
+    dailywts <- setNames(data.frame(matrix(ncol = 2, nrow = length(jan$wthist$date[[i]]))), c("Date", "Weight"))
+    dailywts$Date <- jan$wthist$date[[i]]
+    dailywts$Weight <- jan$wthist$weight[[i]]
 
-  #return(dailywts)
+    if(is.null(start)) {cattleinfo[[jan$RFID[i]]] <- dailywts}
+    else{if(is.null(end)){dailywts <- dailywts %>% filter(between(as.Date(Date),start,Sys.Date()))}
+      else{dailywts <- dailywts %>% filter(between(as.Date(Date),start,end))}}
 
-  dailywts <- dailywts %>% filter(between(as.Date(Date),start,end))
+    cattleinfo[[jan$RFID[i]]] <- dailywts
 
-  return(list(RFID=jan$RFID, Station=jan$stationname, Wts=dailywts))
+
+  }
+
+  return(cattleinfo)
+
 
 }
+
 
 
 
---
+++ R/weeklywts.R
@@ -0,0 +1,37 @@
+#' @import dplyr
+#' @export
+
+
+
+weeklywts <- function(RFID, start=NULL, end=NULL){
+
+  username = keyring::key_list("DMMongoDB")[1,2]
+  password =  keyring::key_get("DMMongoDB")
+
+  pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
+
+  cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
+
+  RFID <- paste(unlist(RFID), collapse = '", "' )
+  filterstation <- sprintf('{"RFID":{"$in":["%s"]}}', RFID)
+  jan2 <- cattle$find(query = filterstation, fields='{"RFID":true, "stationname":true, "wkwthist.date":true, "wkwthist.avweight":true, "_id":false}')
+
+  cattleinfo <- list(RFID=jan2$RFID, Station=jan2$stationname)
+
+  for(i in 1:length(jan2$RFID)){
+    weeklywts <- setNames(data.frame(matrix(ncol = 2, nrow = length(jan2$wkwthist$date[[i]]))), c("Date", "Weight"))
+    weeklywts$Date <- jan2$wkwthist$date[[i]]
+    weeklywts$Weight <- jan2$wkwthist$avweight[[i]]
+
+    if(is.null(start)) {cattleinfo[[jan2$RFID[i]]] <- weeklywts}
+    else{if(is.null(end)){weeklywts <- weeklywts %>% filter(between(as.Date(Date),start,Sys.Date()))}
+      else{weeklywts <- weeklywts %>% filter(between(as.Date(Date),start,end))}}
+
+    cattleinfo[[jan2$RFID[i]]] <- weeklywts
+
+
+  }
+
+  return(cattleinfo)
+
+}
