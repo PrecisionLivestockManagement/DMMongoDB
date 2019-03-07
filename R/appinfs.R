b2e05ed6cb20d5fa48c9af2139ff5b7fd36b4e70
Lauren O'Connor <lauren.oconnor@datamuster.net.au>
2019-03-07 10:29:22 +1000

    Updates to script

--- R/appinfs.R
+++ R/appinfs.R
@@ -5,15 +5,15 @@
 #' @param property the name of the property to search the DataMuster MongoDB Atlas server
 #' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
 #' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
-#' @return a dataframe with a list of the RFID numbers, associated management tags and current paddocks the cattle are in
+#' @return a dataframe with a list of the data numbers, associated management tags and current paddocks the cattle are in
 #' @author Dave Swain \email{dave.swain@@datamuster.net.au} and Lauren O'Connor \email{lauren.oconnor@@datamuster.net.au}
 #' @import mongolite
 #' @import keyring
-#' @import xts
+#' @import dplyr
 #' @export
 
 
-appinfs <- function(property, username=NULL, password=NULL){
+appinfs <- function(property, start=NULL, end=NULL, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
   username = keyring::key_list("DMMongoDB")[1,2]
@@ -26,31 +26,29 @@ appinfs <- function(property, username=NULL, password=NULL){
     verbose = T)
 
   property <- paste(unlist(property), collapse = '", "' )
-  filterstation <- sprintf('{"stationname":{"$in":["%s"]}}', property)
+  filterstation <- sprintf('{"stationname":{"$in":["%s"]}, "properties.type":"%s"}', property, "Walk-over-Weighing Unit")
 
-  assetlist <- list()
+  info <- list()
 
-  tempinfra <- infrastructure$find(query = filterstation, fields='{"properties.asset_id":true, "usehist.date":true, "usehist.num":true, "_id":false}')
-  assetlistname <- list()
-  for(i in 1:nrow(tempinfra)){
-    if(length(tempinfra$usehist$date[[i]][])==0){}else{
-      asset <- as.data.frame(tempinfra$usehist$date[[i]][])
-      asset["Num"] <- as.data.frame(tempinfra$usehist$num[[i]][])
-      asset1 <<- asset
-      colnames(asset) <- c("Date","Num")
-      asset <- xts(asset$Num,asset$Date)
-      assign(as.character(as.character(tempinfra$properties$asset_id[i])), asset, envir = .GlobalEnv)
-      assetlistname <- rbind(as.character(tempinfra$properties$asset_id[i]),assetlistname)
-    }}
+  jan2 <- infrastructure$find(query = filterstation, fields='{"properties.asset_id":true, "usehist.date":true, "usehist.num":true, "_id":false}')
 
-  if(nrow(tempinfra)==0){}else{
-    values$assetlistname <- assetlistname
-    values$asset1 <- asset1}
+  for(i in 1:length(jan2$properties$asset_id)){
 
+    asset <- setNames(data.frame(matrix(ncol = 2, nrow = length(jan2$usehist$date[[i]]))), c("Date", "Num"))
+    asset$Date <- jan2$usehist$date[[i]]
+    asset$Num <- jan2$usehist$num[[i]]
 
+    if(is.null(start)) {}
+    else{if(is.null(end)){asset <- asset %>% filter(between(as.Date(Date),start,Sys.Date()))}
+      else{asset <- asset %>% filter(between(as.Date(Date),start,end))}}
 
-  return(values)
+    info[[jan2$properties$asset_id[i]]] <- as.data.frame(asset)
+    }
+
+  data <- jan2
+  info <- list(data=data$properties$asset_id, UseHist=info)
+
+  return(info)
 
 }
-
 
