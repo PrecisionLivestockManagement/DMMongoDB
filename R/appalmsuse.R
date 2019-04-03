a5f99c533d332e631b3ed3b367f3397ebd7423fb
DavoSwain <d.swain@cqu.edu.au>
2019-04-03 10:52:25 +1000

    Changes to the alms use function plus deleted weekly wts as it was causing some issues.

--- R/appalmsuse.R
+++ R/appalmsuse.R
@@ -2,7 +2,7 @@
 #'
 #' This function provides a list of cattle
 #' usage of an ALMS unit for a property from MongoDB. Inputs need to be a list of one or more property names and if only one property a paddock name can be included
-#' @name ALMSuse
+#' @name appalmsuse
 #' @param property the name of the property to search the DataMuster MongoDB Atlas server
 #' @param paddock this is the name of a paddock or list of paddocks as character entries, if no value is entered then all paddocks are loaded
 #' @param start provide a start date to be returned, this has to be in date format.
@@ -17,13 +17,12 @@
 #' @export
 
 
-ALMSuse <- function(property, paddock=NULL, start=NULL, end=NULL, username = NULL, password = NULL){
+appalmsuse <- function(property, start=NULL, end=NULL, username = NULL, password = NULL){
 
   if(is.null(username)||is.null(password)){
   username = keyring::key_list("DMMongoDB")[1,2]
   password =  keyring::key_get("DMMongoDB", username)
 }
-
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
   inf <- mongo(collection = "Infrastructure", db = "DataMuster", url = pass, verbose = T)
@@ -32,23 +31,22 @@ ALMSuse <- function(property, paddock=NULL, start=NULL, end=NULL, username = NUL
 
   property <- paste(unlist(property), collapse = '", "' )
 
-  if(is.null(paddock)) {
-    filterstation <- sprintf('{"stationname":"%s", "properties.type":"%s"}', property, type)} else {
-    paddock <- paste(unlist(paddock), collapse = '", "' )
-    filterstation <- sprintf('{"properties.Paddock":{"$in":["%s"]}, "stationname":"%s", "properties.type":"%s"}', paddock, property, type)}
 
+  filterstation <- sprintf('{"stationname":{"$in":["%s"]}, "properties.type":"%s"}', property, type)
   lookfor <- sprintf('{"_id":false, "stationname":true, "properties.asset_id":true, "properties.Paddock":true,
-                     "usehist.date":true, "usehist.num":true, "usehist.numbreed":true, "usehist.numgrow_w":true, "usehist.numgrow_uw":true,
-                     "cattlehist.date":true, "cattlehist.num":true, "cattlehist.numbreed":true, "cattlehist.numgrow_w":true, "cattlehist.numgrow_uw":true}')
+    "usehist.date":true, "usehist.num":true, "usehist.numbreed":true, "usehist.numgrow_w":true, "usehist.numgrow_uw":true,
+    "cattlehist.date":true, "cattlehist.num":true, "cattlehist.numbreed":true, "cattlehist.numgrow_w":true, "cattlehist.numgrow_uw":true}')
 
   jan2 <- inf$find(query = filterstation, fields=lookfor)
-
 
   cattleinfo <- list()
 
   for(i in 1:length(jan2$properties$asset_id)){
 
-    dailywts <- setNames(data.frame(matrix(NA, ncol = 5, nrow = length(jan2$usehist$date[[i]]))), c("Date", "GroupCount", "BreedingCount", "Growing_WeanedCount", "Growing_UnweanedCount"))
+    dailywts <- setNames(data.frame(matrix(NA, ncol = 8, nrow = length(jan2$usehist$date[[i]]))), c("Property", "Paddock", "ID", "Date", "GroupCount", "BreedingCount", "Growing_WeanedCount", "Growing_UnweanedCount"))
+    if(length(jan2$usehist$date[[i]])==0){}else {dailywts$Property <- jan2$stationname[i]}
+    if(length(jan2$usehist$date[[i]])==0){}else {dailywts$Paddock <- jan2$properties$Paddock[i]}
+    if(length(jan2$usehist$date[[i]])==0){}else {dailywts$ID <- jan2$properties$asset_id[i]}
     if(length(jan2$usehist$date[[i]])==0){}else {dailywts$Date <- jan2$usehist$date[[i]]}
     if(length(jan2$usehist$num[[i]])==0){}else{dailywts$GroupCount <- jan2$usehist$num[[i]]}
     if(length(jan2$usehist$numbreed[[i]])==0){}else{dailywts$BreedingCount <- jan2$usehist$numbreed[[i]]}
@@ -62,15 +60,7 @@ ALMSuse <- function(property, paddock=NULL, start=NULL, end=NULL, username = NUL
     if(length(jan2$cattlehist$numgrow_w[[i]])==0){}else{dailywts1$Growing_Weaned <- jan2$cattlehist$numgrow_w[[i]]}
     if(length(jan2$cattlehist$numgrow_uw[[i]])==0){}else{dailywts1$Growing_Unweaned <- jan2$cattlehist$numgrow_uw[[i]]}
 
-    if (nrow(dailywts) == 0) {} else {
-
-      if (nrow(dailywts1) == 0) {
-        dailywts1 <- setNames(data.frame(matrix(NA, ncol = 5, nrow = length(jan2$usehist$date[[i]]))), c("Date", "Group", "Breeding",  "Growing_Weaned", "Growing_Unweaned"))
-        dailywts1$Date <- jan2$usehist$date[[i]]}
-
-      use <- merge.data.frame(dailywts1, dailywts, by = "Date", all = T)}
-
-    if(exists("use")){
+    use <- merge.data.frame(dailywts, dailywts1, by = "Date", all = T)
 
     if(is.null(start)) {} else {
       if(is.null(end)){use <- use %>% filter(between(as.Date(Date),start-1,Sys.Date()))} else{
@@ -78,12 +68,8 @@ ALMSuse <- function(property, paddock=NULL, start=NULL, end=NULL, username = NUL
 
     cattleinfo[[jan2$properties$asset_id[i]]] <- as.data.frame(use)
 
-
-  asset <- jan2
-  cattleinfo <- list(asset=asset$properties$asset_id, Paddock = asset$properties$Paddock, Property=asset$stationname, DailyUse=cattleinfo)
-
-  return(cattleinfo)
-}else{return(NULL)}
-
   }
+
+  cattleinfo <- list(asset=jan2$properties$asset_id, Paddock = jan2$properties$Paddock, Property=jan2$stationname, DailyUse=cattleinfo)
+
 }
