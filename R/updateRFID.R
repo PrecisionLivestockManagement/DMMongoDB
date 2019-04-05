cc0db7bf4d35a0459c681310f1b6cdc0a4d112f9
Lauren O'Connor <lauren.oconnor@datamuster.net.au>
2019-04-05 15:07:47 +1000

    New function to update RFID number

---
+++ R/updateRFID.R
@@ -0,0 +1,79 @@
+#' Update RFID tag number
+#'
+#' This function pulls in daily weights for individual or groups of cattle for specified periods. It searches based on a list of RFID values. It is recommended that you use the propsearch function to find a list of cattle RFID numbers for a particular property. If you need assistance please email \email{info@@datamuster.net.au} to seek help or suggest improvements.
+#' @name updateRFID
+#' @param RFID this is a list of the previous cattle RFID numbers
+#' @param newRFID this is a list of the new cattle RFID numbers
+#' @param date provide the date that the new RFID tag was applied, this has to be in date format. Default is today's date.
+#' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
+#' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
+#' @return a message that indicates the RFID tag number has been successfully updated
+#' @author Dave Swain \email{dave.swain@@datamuster.net.au} and Lauren O'Connor \email{lauren.oconnor@@datamuster.net.au}
+#' @import mongolite
+#' @import keyring
+#' @import dplyr
+#' @export
+
+updateRFID <- function(RFID, newRFID, date=NULL, username=NULL, password=NULL){
+
+  if(is.null(username)||is.null(password)){
+  username = "LaurenOconnor"
+  password =  "DataMuster"}
+
+  pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
+  cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
+
+  if(is.null(date)){date <- Sys.Date()}else{date <- as.POSIXct(date)}
+
+  if (length(date) == 1){date <- rep(date, length = length(RFID))}
+
+  RFID <- paste(unlist(RFID), collapse = '", "' )
+  newRFID <- paste(unlist(newRFID), collapse = '", "' )
+
+  # Checks that the RFID numbers are in the correct format for the database
+
+  if("TRUE" %in% (nchar(as.character(RFID))!= 16)) {
+      stop(paste0("One or more of the previous RFID numbers are not in the correct format. Please ensure all RFIDs are in the format 'xxx xxxxxxxxxxxx'"))}
+
+  if("TRUE" %in% (nchar(as.character(newRFID))!= 16)) {
+    stop(paste0("One or more of the new RFID numbers are not in the correct format. Please ensure all RFIDs are in the format 'xxx xxxxxxxxxxxx'"))}
+
+
+  # Checks that the old RFID numbers exist in the database
+
+  filtercattle <- sprintf('{"RFID":{"$in":["%s"]}}', RFID)
+  check <- cattle$count(query = filtercattle)
+
+  if (check != length(RFID)) {
+
+    stop("One or more of the previous RFID numbers cannot be found in the database. Please check that the RFID numbers are correct and try again")}
+
+
+  # Checks that the new RFID numbers are not already registered in the database
+
+  filtercattle <- sprintf('{"RFID":{"$in":["%s"]}}', newRFID)
+  check <- cattle$count(query = filtercattle)
+
+  if (check != 0) {
+
+    stop("One or more of the new RFID numbers are already registered in the database. Please check that the RFID numbers are correct and try again")}
+
+
+
+  for (i in 1:length(RFID)){
+
+   RFIDS <- sprintf('{"RFID":"%s"}', RFID[i])
+
+          banger <- cattle$find(query = RFIDS, fields='{"RFIDhist.date":true, "_id":false}')
+          arrpos <- length(banger$RFIDhist$date[[1]])
+          RFIDIlast <- sprintf('{"$set":{"RFID":"%s"}}', newRFID[i])
+          RFIDI <- sprintf('{"$set":{"RFIDhist.date.%s":{"$date":"%s"}, "RFIDhist.ID.%s":"%s"}}', arrpos, paste0(substr(date[i],1,10),"T","00:00:00","+1000"), arrpos, newRFID[i])
+
+      cattle$update(RFIDS, RFIDI) # Has to update RFIDI first otherwise won't update RFIDhist
+      cattle$update(RFIDS, RFIDIlast)
+
+  }
+
+  }
+
+
