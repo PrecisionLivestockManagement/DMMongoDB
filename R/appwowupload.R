09ce52ebf645712aa99edd6933729489b3b889c9
Lauren O'Connor <l.r.williams@cqu.edu.au>
2019-08-01 11:08:51 +1000

    Update to appwowupload function

--- R/appwowupload.R
+++ R/appwowupload.R
@@ -14,12 +14,17 @@
 #' @export
 
 
-appwowupload <- function(RFID, datetime, weight, ALMS, username, password){
+appwowupload <- function(RFID, datetime, weight, ALMS, username=NULL, password=NULL){
+
+  if(is.null(username)||is.null(password)){
+    username = keyring::key_list("DMMongoDB")[1,2]
+    password =  keyring::key_get("DMMongoDB", username)
+  }
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   wowdata <- mongo(collection = "WoWData", db = "DMIoT", url = pass, verbose = T)
 
-  data <- sprintf('{"RFID":"%s", "datetime":{"$date":"%s"}, "Wt":%s, "Location": %s , "createdAt": %s }', RFID, paste0(substr(datetime,1,10),"T",substr(datetime,12,19),"+1000"), weight, ALMS, system.time())
+  data <- sprintf('{"RFID":"%s", "datetime":{"$date":"%s"}, "Wt":%s, "Location": "%s" , "createdAt":{"$date":"%s"}}', RFID, paste0(substr(datetime,1,10),"T",substr(datetime,12,19),"+1000"), weight, ALMS, paste0(substr(Sys.time(),1,10),"T",substr(Sys.time(),12,19),"+1000"))
 
   wowdata$insert(data)
 
