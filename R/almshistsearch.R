b7e7878be5b46437127c8197f24ce2eb9412b289
Lauren O'Connor <l.r.williams@cqu.edu.au>
2019-09-24 08:16:58 +1000

    Updates to function

--- R/almshistsearch.R
+++ R/almshistsearch.R
@@ -17,36 +17,42 @@
 #' @export
 
 
-almshistsearch <- function(property, alms=NULL, start=NULL, end=NULL, username=NULL, password=NULL){
+almshistsearch <- function(property=NULL, alms=NULL, start=NULL, end=NULL, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
   username = keyring::key_list("DMMongoDB")[1,2]
   password =  keyring::key_get("DMMongoDB", username)
   }
 
-  if(is.null(start)){start <- as.Date("1970-01-01")}
+  if(is.null(start)){start <- as.Date("2015-01-01")}
   if(is.null(end)){end <- Sys.Date()}
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
 
-  property <- paste(unlist(property), collapse = '", "' )
+  #property <- paste(unlist(property), collapse = '", "' )
   #filterstation1 <- sprintf('{"stationname":{"$in":["%s"]}}', property)
-  filterstation2 <- sprintf('{"$or": [{"exstation":"%s"}, {"stationname":"%s"}]}', property, property)
+  #filterstation2 <- sprintf('{"$or": [{"exstation":"%s"}, {"stationname":"%s"}]}', property, property)
 
-  lookfor <- sprintf('{"RFID":true, "stationname":true, "almshist.asset_id":true, "almshist.dateON":true, "almshist.dateOFF":true, "_id":false}')
+  lookfor <- sprintf('{"RFID":true, "stationname":true, "exstation":true, "almshist.asset_id":true, "almshist.dateON":true, "almshist.dateOFF":true, "_id":false}')
 
-  jan2 <- cattle$find(query = filterstation2, fields=lookfor)
+  #jan2 <- cattle$find(query = filterstation2, fields=lookfor)
+  jan2 <- cattle$find(query = '{}', fields=lookfor)
+
+  if(is.null(property)){}else{
+    jan2 <- jan2%>%
+    filter(stationname %in% property | exstation %in% property)}
 
   cattleinfo <- list()
 
   for(i in 1:length(jan2$RFID)){
 
-    dailywts <- setNames(data.frame(matrix(ncol = 3, nrow = length(jan2$almshist$asset_id[[i]]))), c("ALMS", "dateON", "dateOFF"))
+    dailywts <- setNames(data.frame(matrix(ncol = 4, nrow = length(jan2$almshist$asset_id[[i]]))), c("Property","ALMS", "dateON", "dateOFF"))
 
     if (nrow(dailywts) >= 1){
       dailywts$dateON <- as.Date(jan2$almshist$dateON[[i]], tz = "Australia/Brisbane")
       dailywts$ALMS <- jan2$almshist$asset_id[[i]]
+      dailywts$Property <- ifelse(jan2$stationname[i] == "xxxxxx", jan2$exstation[i], jan2$stationname[i])
       if (length(jan2$almshist$dateON[[i]]) != 0){
         if (length(jan2$almshist$dateOFF[[i]]) != nrow(dailywts)){
           if(length(jan2$almshist$dateOFF[[i]]) == 0) {
@@ -69,7 +75,10 @@ almshistsearch <- function(property, alms=NULL, start=NULL, end=NULL, username=N
 
           if (length(dates) == 0){dailywts$ALMS[n[p]] <- NA}}}
 
+      if(is.null(alms)){}else{
       dailywts <- dailywts %>% filter(!(is.na(ALMS)), ALMS %in% alms)}
+
+      }
 
 
     #This is the section where we can apply further filters based on breed, class, etc.
@@ -80,7 +89,8 @@ almshistsearch <- function(property, alms=NULL, start=NULL, end=NULL, username=N
   }
 
   RFID <- jan2[which(jan2$RFID!="xxxx"),]
-  cattleinfo <- list(RFID=RFID$RFID, ALMSHistory=cattleinfo)
+  #RFID$Property <- ifelse(RFID$stationname == "xxxxxx", RFID$exstation, RFID$stationname)
+  cattleinfo <- list(RFID=RFID$RFID, Property=RFID$Property, ALMSHistory=cattleinfo)
 
   return(cattleinfo)
 
