eab599c3d7e7205bc76de6f3dfe76797ca0ab5ac
Lauren O'Connor <l.r.williams@cqu.edu.au>
2019-10-16 08:17:04 +1000

    Amended script to read the simulated polygon in the stations database if no paddocks are mapped.

--- R/apppaddocks.R
+++ R/apppaddocks.R
@@ -19,33 +19,54 @@
 
 apppaddocks <- function(property, username=NULL, password=NULL){
 
-  filterstation <- sprintf('{"stationname":"%s"}', property)
-
   if(is.null(username)||is.null(password)){
   username = keyring::key_list("DMMongoDB")[1,2]
   password =  keyring::key_get("DMMongoDB", username)
 }
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
-  paddocks <- mongo(collection = "Paddocks", db = "DataMuster",
-    url = pass,
-    verbose = T)
+  paddocks <- mongo(collection = "Paddocks", db = "DataMuster", url = pass, verbose = T)
+  stations <- mongo(collection = "Stations", db = "DataMuster", url = pass, verbose = T)
 
+  property1 <- paste(unlist(property), collapse = '", "' )
 
-  property <- paste(unlist(property), collapse = '", "' )
-  filterstation <- sprintf('{"stationname":{"$in":["%s"]}}', property)
+  filterpaddocks <- sprintf('{"stationname":{"$in":["%s"]}}', property1)
 
-  tempadds <- paddocks$find(query = filterstation, fields='{"stationname":true, "geometry":true, "properties.hectares":true, "paddname":true, "_id":false}')
+  tempadds <- paddocks$find(query = filterpaddocks, fields='{"stationname":true, "geometry":true, "properties.hectares":true, "paddname":true, "_id":false}')
 
-  cattle = SpatialPolygons(lapply(1:nrow(tempadds), function(x) Polygons(list(Polygon(matrix(tempadds$geometry$coordinates[[x]], ncol = 2))), paste0("ID",x))), proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+  if(nrow(tempadds) != 0){tempadds <- tempadds%>%
+                                      mutate(hectares = properties$hectares,
+                                             geom = geometry$coordinates)%>%
+                                      select("stationname", "paddname", "hectares", "geom")}else{
 
-  cattleadd <- tempadds%>%select(stationname,paddname)
-  cattleadd["hectares"] <- tempadds$properties$hectares
+                                        tempadds <- data.frame()}
+
+  missingprops <- property[!(property %in% tempadds$stationname)]
+
+  if(length(missingprops)!=0){
+
+    property2 <- paste(unlist(missingprops), collapse = '", "' )
+
+    filterstations <- sprintf('{"name":{"$in":["%s"]}}', property2)
+
+    tempadds1 <- stations$find(query = filterstations, fields='{"name":true, "geometry":true, "hectares":true, "_id":false}')
+
+    tempadds1 <- tempadds1%>%
+                 mutate(paddname = "1",
+                        geom = geometry$coordinates)%>%
+                 rename(stationname = name)%>%
+                  select("stationname", "paddname", "hectares", "geom")
+
+    tempadds <- rbind(tempadds, tempadds1)
+  }
+
+  cattle = SpatialPolygons(lapply(1:nrow(tempadds), function(x) Polygons(list(Polygon(matrix(tempadds$geom[[x]], ncol = 2))), paste0("ID",x))), proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+  cattleadd <- tempadds%>%select(stationname, paddname, hectares)
+
 
   PropPadds <- SpatialPolygonsDataFrame(cattle, cattleadd, match.ID=FALSE)
 
-
   return(PropPadds)
-
 
 }
