737d0e68de5b111f0c26b27a13a1f3c284514b1c
Lauren O'Connor <l.r.williams@cqu.edu.au>
2019-10-21 10:05:07 +1000

    Updated function to upload kmz files

--- R/addnewpaddock.R
+++ R/addnewpaddock.R
@@ -3,9 +3,9 @@
 #' This function adds a new paddock or paddocks to the DataMuster database. If you need assistance please email \email{info@@datamuster.net.au} to seek help or suggest improvements.
 #' @name addnewpaddock
 #' @param property this is the name of the property
-#' @param paddockname this is the name of the paddock or paddocks
+#' @param paddockname this is the name of the paddock or paddocks. If NULL, the paddock will be named as the paddock number
 #' @param filedir the location of the spatial file
-#' @param filename the name of the spatial file
+#' @param filename the name of the spatial file. If NULL, all spatial files in the directory will be read and added as paddocks
 #' @param filetype the type of spatial file (e.g. shp, kmz)
 #' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
 #' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
@@ -15,9 +15,10 @@
 #' @import keyring
 #' @import sf
 #' @import rgdal
+#' @import leafletR
 #' @export
 
-addnewpaddock <- function(property, paddockname, filedir, filename, filetype, username=NULL, password=NULL){
+addnewpaddock <- function(property, filedir, filetype, paddockname=NULL, filename=NULL, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
     username = keyring::key_list("DMMongoDB")[1,2]
@@ -31,65 +32,59 @@ addnewpaddock <- function(property, paddockname, filedir, filename, filetype, us
     propertyid <- propertyinfo$`_id`
 
     template <- paddocks$find(query = '{"stationname":"xxxxxx"}', fields = '{"_id":false}')
-    count <- paddocks$count(query = sprintf('{"stationname":"%s"}', property))
-
     template$stationname <- property
     template$stationID <- propertyid
-    template$paddname <- paddockname
     template$properties$datasource <- filetype
-    template$paddnum <- count+1
+
+    rownames(template)<-c()
+    rownames(template$geometry)<-c()
+
+    setwd(filedir)
 
     #Read in file
     #Need to rename kmz as .kml.zip, unzip the folder then read the doc.kml file using st_read
 
     if(filetype == "kmz"){
 
-    setwd(filedir)
+      if(is.null(filename)){filename <- list.files(pattern = ".kmz")}
 
-    unzip(filename)
+   for (i in 1:length(filename)){
 
-    #file <- st_read("doc.kml")
+     file <- filename[i]
+     unzip(file)
 
+    lyr <- ogrListLayers("doc.kml")
 
     foo = readOGR("doc.kml", lyr[1])
 
     pads = toGeoJSON(data=foo, name = "data", dest=tempdir())
 
-    #file <- file[3]
-
-    #file$geometry[[1]][[1]] <- matrix(file$geometry[[1]][[1]][file$geometry[[1]][[1]] != 0], ncol = 2)
-
-    #file[2:7] <- template$properties
-
-    #PropPadds <- st_transform(file, CRS("+proj=longlat +datum=WGS84 +no_defs"))
-
-    # Output spatial dataframe as a geojson file with the MongoDB format, rather than worrying about directories etc just create a temporary file
-    #pads = tempfile(fileext=".geojson")
-
-    # Write the spatial dataframe to the temporary geojson file
-    #st_write(PropPadds, pads, "GeoJSON", driver="GeoJSON")
-    }
-
     # When rgdal writes the geojson file it includes header information and commas at the end of lines - if we want to continue to have each document representing
     # a paddock we need to clean up the geojson file. I think this is the best way to go but maybe we need to discuss maintaining paddocks at the property level.
 
     jack <- readLines(pads)
-    jack <- jack[-c(1:5)]
+    jack <- jack[-c(1:12)]
+    jack <- jack[-length(jack)]
+    jack <- jack[-length(jack)]
     jack <- jack[-length(jack)]
     jack <- jack[-length(jack)]
 
-    for (i in 1:length(jack)) {
+    jack1<-jack[1]
 
-      n <- count+i
+    jack2 <- substr(jack1, 27, nchar(jack1)-4)
 
-      jack[i] <- gsub ('^(.{20})(.*)$', paste0("\\1 \"stationname\": \"",property,"\", \"stationID\": \"" , propertyid,"\",  \"paddname\": \"",paddockname,"\",\"paddnum\": \"", n,"\",\\2"),jack[i])
+    count <- paddocks$count(query = sprintf('{"stationname":"%s"}', property))
 
-      jack[i] <- gsub(gsub (".*paddnum\":\\s*|,.*","",jack[i]), n, jack[i])
+    template$paddname <- ifelse(is.null(paddockname),as.character(count+1), paddockname)
 
-      if (i != length(jack)) {jack[i] <- gsub ( '.{1}$','',jack[i])}
+    template$paddnum <- as.integer(count+1)
 
-    }
+    paddocks$insert(template)
 
-    paddocks$insert(jack)
+    IDS <- sprintf('{"stationname":"%s", "paddnum":%s}', property, template$paddnum)
+    IDI <- sprintf('{"$set":{"geometry.coordinates":[[%s]]}}', jack2)
+    paddocks$update(IDS, IDI)
+   }
+      }
 
 }
