ca649258f81e43804fdcd41a93a9406c08ebecc7
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-01-24 14:37:26 +1000

    Added a unit variable to select data from specific units

--- R/dailywtsNEW.R
+++ R/dailywtsNEW.R
@@ -6,6 +6,7 @@
 #' @param start a start date to be returned in date format.
 #' @param end an end date to be returned in date format
 #' @param values the minimum number of weight values required, default is 1
+#' @param unit the filename of a specific ALMS unit to search
 #' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
 #' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
 #' @return a list of cattle RFID numbers and associated daily weights and dates. Please note daily ALMS records are not reliable indicators of cattle weights, the weekly average data is much more reliable. However, the daily values can be useful to identify fine scale changes in weight associated with calving events. The daily data is also available for researchers to identify improved weekly averaging algorithms based on the daily data.
@@ -16,8 +17,7 @@
 #' @export
 
 
-dailywtsNEW <- function(RFID, start=NULL, end=NULL, values=NULL, username=NULL, password=NULL){
-
+dailywtsNEW <- function(RFID, start=NULL, end=NULL, values=NULL, unit=NULL, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
   username = keyring::key_list("DMMongoDB")[1,2]
@@ -46,10 +46,12 @@ dailywtsNEW <- function(RFID, start=NULL, end=NULL, values=NULL, username=NULL, 
 
   filterwts <- sprintf('{"RFID":{"$in":["%s"]}}', rfids)
 
-  wts <- dailywts$find(query = filterwts, fields= '{"RFID":true, "Wt":true, "datetime":true, "_id":false}')
+  wts <- dailywts$find(query = filterwts, fields= '{"RFID":true, "Wt":true, "datetime":true, "Location":true, "_id":false}')
 
   if(is.null(start)){}else{if(is.null(end)){wts <- wts %>% filter(between(as.Date(datetime, tz = "Australia/Brisbane"),start,Sys.Date()))}
       else{wts <- wts %>% filter(between(as.Date(datetime, tz = "Australia/Brisbane"),start,end))}}
+
+  if(is.null(unit)){}else{wts <- wts %>% filter(Location %in% unit)}
 
   cattlewts <- setNames(data.frame(matrix(ncol = 2, nrow = nrow(wts))), c("Date", "Weight"))
 
