56f8ad16846fb6a45dd905223db6fb3aefc396f8
DavoSwain <dave.swain@terracipher.com>
2020-04-17 12:13:27 +1000

    adding new function for raw WoW data

---
+++ R/add_rawWoW.R
@@ -0,0 +1,32 @@
+#' Add GPS data to the DataMuster MongoDB database
+#'
+#' This function adds GPS data to the DataMuster MongoDB database. If you need assistance please email \email{info@@datamuster.net.au} to seek help or suggest improvements.
+#' @name add_gps
+#' @param RFID a list of cattle RFID number/s
+#' @param DateTime a list of timestamps in POSIXct format
+#' @param lat the latitude of a coordinate point
+#' @param long the longitude of a coordinate point
+#' @param voltage the voltage of the device as a number or percentage
+#' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
+#' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
+#' @return a message that indicates the data has been successfully added
+#' @author Dave Swain \email{d.swain@@cqu.edu.au} and Lauren O'Connor \email{l.r.oconnor@@cqu.edu.au}
+#' @import mongolite
+#' @export
+
+
+add_gps <- function(RFID){
+
+
+  pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
+  GPSIoT <- mongo(collection = "WoWData", db = "WoWRaw",
+    url = pass,
+    verbose = T)
+
+  GPSdata <- sprintf('{"RFID":"%s", "DateTime":{"$date":"%s"}, "lat":%s, "long": %s , "voltage": %s }', RFID, paste0(substr(DateTime,1,10),"T",substr(DateTime,12,19),"+1000"), lat, long, voltage)
+
+  GPSIoT$insert(GPSdata)
+
+
+
+}
