3a999cda322296a1af16a1bba7259e64b7f43c07
Lauren O'Connor <l.r.williams@cqu.edu.au>
2020-05-07 10:37:34 +1000

    Added management tag as a search term

--- R/get_cattle.R
+++ R/get_cattle.R
@@ -29,7 +29,7 @@
 #' @export
 
 
-get_cattle <- function(RFID = NULL, property = NULL, sex = NULL, category = NULL, paddock = NULL, alms = NULL, weaned = NULL, id = NULL, almsasset_id = NULL, exstation = NULL, exitdate = NULL, entrydate = NULL, deathdate = NULL, timezone = NULL, prevpaddock = NULL, active = NULL, fields = NULL, username = NULL, password = NULL){
+get_cattle <- function(RFID = NULL, MTag = NULL, property = NULL, sex = NULL, category = NULL, paddock = NULL, alms = NULL, weaned = NULL, id = NULL, almsasset_id = NULL, exstation = NULL, exitdate = NULL, entrydate = NULL, deathdate = NULL, timezone = NULL, prevpaddock = NULL, active = NULL, fields = NULL, username = NULL, password = NULL){
 
   if(is.null(username)||is.null(password)){
     username = keyring::key_list("DMMongoDB")[1,2]
@@ -51,6 +51,7 @@ get_cattle <- function(RFID = NULL, property = NULL, sex = NULL, category = NULL
   if(is.null(prevpaddock)){} else {prevpaddock <- sprintf('"properties.PrevPaddock":"%s",', prevpaddock)}
   if(is.null(active)){} else {active <- sprintf('"active":"%s",', active)}
   if(is.null(id)){} else {id <- sprintf('"_id":{"$oid":"%s"},', id)}
+  if(is.null(MTag)){} else {MTag <- sprintf('"properties.Management":"%s",', MTag)}
 
   if(is.null(exitdate)){} else {exitdate <- sprintf('"properties.exitDate":{"$gte":{"$date":"%s"}},', strftime(paste0(exitdate, "00:00:00"), format="%Y-%m-%dT%H:%M:%OSZ", tz = "GMT"))}
   if(is.null(entrydate)){} else {entrydate <- sprintf('"properties.entryDate":{"$gte":{"$date":"%s"}},', strftime(paste0(entrydate, "00:00:00"), format="%Y-%m-%dT%H:%M:%OSZ", tz = "GMT"))}
@@ -72,7 +73,7 @@ cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = 
 
 # Set up find query
 
-search <-paste0("{", property, sex, paddock, category, alms, weaned, id, almsasset_id, exstation, exitdate, entrydate, deathdate, RFID, prevpaddock, active,"}")
+search <-paste0("{", property, sex, paddock, category, alms, weaned, id, almsasset_id, exstation, exitdate, entrydate, deathdate, RFID, MTag, prevpaddock, active,"}")
 
 if(nchar(search)==2){}else{
 search <- substr(search, 1 , nchar(search)-2)
