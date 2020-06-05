5892934602aee51d455153bb7c94b08b3856e974
Anita Chang <a.chang@cqu.edu.au>
2020-06-05 13:02:31 +1000

    Modified to automatically calculate estimatedcalvingdate

--- R/add_foetalagedata.R
+++ R/add_foetalagedata.R
@@ -2,21 +2,20 @@
 #'
 #' This function updates individual or groups of cow calving information in the DataMuster MongoDB database. If you need assistance please email \email{info@@datamuster.net.au} to seek help or suggest improvements.
 #' @name add_foetalagedata
-#' @param MTag a list of cattle management tag number/s
-#' @param property the name of the property
-#' @param calfMTag the calf's management tag number
-#' @param birthDate the cow's date of calving in date format
+#' @param RFID a list of cattle RFID number/s
+#' @param foetalagedate the date of scanning in date format
+#' @param foetalage the foetal age at scanning
 #' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
 #' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
 #' @return a message that indicates that the calving information has been successfully updated
-#' @author Dave Swain \email{d.swain@@cqu.edu.au} and Lauren O'Connor \email{l.r.oconnor@@cqu.edu.au}
+#' @author Dave Swain \email{d.swain@@cqu.edu.au}, Lauren O'Connor \email{l.r.oconnor@@cqu.edu.au}, and Anita Chang \email{a.chang@@cqu.edu.au}
 #' @import mongolite
 #' @import keyring
 #' @import dplyr
 #' @export
 
 
-add_foetalagedata <- function(RFID, foetalagedate, foetalage, estcalvingdate, username=NULL, password=NULL){
+add_foetalagedata <- function(RFID, foetalagedate, foetalage, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
     username = keyring::key_list("DMMongoDB")[1,2]
@@ -27,6 +26,8 @@ add_foetalagedata <- function(RFID, foetalagedate, foetalage, estcalvingdate, us
     cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
     calvingdata <- mongo(collection = "CalvingData", db = "DataMuster", url = pass, verbose = T)
 
+    estcalvingdate <- (40 - foetalage)
+    estcalvingdate <- foetalagedate + estcalvingdate*7
 
 # Find cows in the database
 
@@ -51,8 +52,8 @@ add_foetalagedata <- function(RFID, foetalagedate, foetalage, estcalvingdate, us
 
     #  Update CalvingData collection --------------------------
 
-    data <- data.frame(MTag, property, foetalagedate, foetalage, estcalvingdate, stringsAsFactors = F)
-    data <- left_join(data, cows, by = "MTag")
+    data <- data.frame(RFID, foetalagedate, foetalage, estcalvingdate, stringsAsFactors = F)
+    data <- left_join(data, cows, by = "RFID")
 
     template <- calvingdata$find(query = sprintf('{"RFID":"xxxxxx"}'), fields = '{"_id":false}')
 
@@ -76,10 +77,11 @@ add_foetalagedata <- function(RFID, foetalagedate, foetalage, estcalvingdate, us
 
         RFIDS <- sprintf('{"RFID":"%s"}', template$RFID[i])
 
-        RFIDI <- sprintf('{"$set":{"properties.foetalagedate":{"$date":"%s"}, "foetalage":%s, "properties.estcalvingdate":{"$date":"%s"}}}',
+
+        RFIDI <- sprintf('{"$set":{"preghist.date.%s":{"$date":"%s"}, "preghist.foetalage.%s":%s, "preghist.estcalvingdate.%s":{"$date":"%s"}}}',
                          paste0(substr(template$foetalagedate[i],1,10),"T","00:00:00","+1000"), template$foetalage[i], paste0(substr(template$estcalvingdate[i],1,10),"T","00:00:00","+1000"))
 
-        cattle$update(IDS, RFIDI)
+        cattle$update(RFIDS, RFIDI)
 
 
       }
