f3a65ed076f7db6637719dda2ebdd0927b56e537
Anita Chang <a.chang@cqu.edu.au>
2021-05-20 12:44:46 +1000

    Autodrafting functions

--- R/update_autodraft.R
+++ R/update_autodraft.R
@@ -1,9 +1,8 @@
-#' Update autodrafter details or remove animals from the autodrafter in the DataMuster MongoDB database.
+#' Update autodrafter details in the DataMuster MongoDB database.
 #'
-#' This function updates the autodrafter details or removes individual or groups of cattle in the DataMuster MongoDB database. If you need assistance please email \email{info@@datamuster.net.au} to seek help or suggest improvements.
+#' This function updates the autodrafter details for individual or groups of cattle in the DataMuster MongoDB database. If you need assistance please email \email{info@@datamuster.net.au} to seek help or suggest improvements.
 #' @name update_autodraft
 #' @param RFID a list of the previous cattle RFID number/s
-#' @param active whether the autodrafter is still active
 #' @param alms the new ALMS ID
 #' @param direction the direction of the autodrafter
 #' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
@@ -15,7 +14,7 @@
 #' @import dplyr
 #' @export
 
-update_autodraft <- function(RFID, active = NULL, alms = NULL, direction = NULL, username=NULL, password=NULL){
+update_autodraft <- function(RFID, alms = NULL, direction, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
     username = keyring::key_list("DMMongoDB")[1,2]
@@ -24,19 +23,23 @@ update_autodraft <- function(RFID, active = NULL, alms = NULL, direction = NULL,
 
   pass <- sprintf("mongodb://%s:%s@datamuster-shard-00-00-8mplm.mongodb.net:27017,datamuster-shard-00-01-8mplm.mongodb.net:27017,datamuster-shard-00-02-8mplm.mongodb.net:27017/test?ssl=true&replicaSet=DataMuster-shard-0&authSource=admin", username, password)
   cattle <- mongo(collection = "Cattle", db = "DataMuster", url = pass, verbose = T)
+  autodraft <- mongo(collection = "AutoDraft", db = "DataMuster", url = pass, verbose = T)
+  adhist <- mongo(collection = "AutoDraftHistory", db = "DataMuster", url = pass, verbose = T)
 
   # Checking RFID is in the correct format
   checkRFID <- paste(unlist(RFID), collapse = '", "' )
   if("TRUE" %in% (nchar(as.character(RFID))!= 16)) {
     stop(paste0("One or more of the previous RFID numbers are not in the correct format. Please ensure all RFIDs are in the format 'xxx xxxxxxxxxxxx'"))}
 
+  ##### Update Cattle collection #####
   for(i in 1:length(RFID)){
     RFIDS <- sprintf('{"RFID":"%s"}', RFID[i])
-    finder <- cattle$find(query = RFIDS,
+    cattfinder <- cattle$find(query = RFIDS,
                           fields = '{"_id":false, "RFID":true, "properties.AUTODRAFT":true, "properties.AUTODRAFTasset_id":true, "properties.AUTODRAFT_dir":true}')
-    if(!is.null(active)){
-      activeer <- sprintf('{"$set":{"properties.AUTODRAFT":"%s"}}', active[i])
-      cattle$update(RFIDS, activeer)
+
+    if(!is.null(direction)){
+      directioner <- sprintf('{"$set":{"properties.AUTODRAFT_dir":"%s"}}', direction[i])
+      cattle$update(RFIDS, directioner)
     }
 
     if(!is.null(alms)){
@@ -44,10 +47,24 @@ update_autodraft <- function(RFID, active = NULL, alms = NULL, direction = NULL,
       cattle$update(RFIDS, almser)
     }
 
+  ##### Update AutoDraft collection #####
+    autofinder <- autodraft$find(query = RFIDS,
+                                 fields = '{"_id":false, "RFID":true, "mtag":true, "property":true, "paddock":true,
+                                 "asset_id":true, "direction":true}')
+
     if(!is.null(direction)){
-      directioner <- sprintf('{"$set":{"AUTODRAFT_dir":"%s"}}', direction[i])
-      cattle$update(RFIDS, directioner)
+      directioner <- sprintf('{"$set":{"direction":"%s"}}', direction[i])
+      autodraft$update(RFIDS, directioner)
     }
+
+    if(!is.null(alms)){
+      almser <- sprintf('{"$set":{"asset_id":"%s"}}', alms[i])
+      autodraft$update(RFIDS, almser)
+    }
+
+  ##### Update AutoDraftHistory collection #####
+    template <- data.frame(autofinder[,-c(5,6)], asset_id = alms[i], direction = direction[i], date = Sys.time())
+    adhist$insert(template)
   }
 
   }
