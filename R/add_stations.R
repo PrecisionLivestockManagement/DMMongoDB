fe3db480b1aea7982fe68e11047de8862056a7f2
Lauren O'Connor <l.r.oconnor@cqu.edu.au>
2023-01-05 16:45:35 +1000

    Removing PIC number from function input

--- R/add_stations.R
+++ R/add_stations.R
@@ -7,7 +7,6 @@
 #' @param long the longitude of a coordinate point to locate the property
 #' @param area the area of the staion in hectares, default is 100 ha
 #' @param timezone the local timezone of the property, see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for the list of accepted timezones, default is Australia/Brisbane
-#' @param PIC the Property Identification Code
 #' @param username if you don't have a username set up using the dmaccess function you can pass a username, if no value added then the function looks for a value from dmaccess via keyring
 #' @param password if you include a username you will also need to add a password contact Lauren O'Connor if you don't have access
 #' @return a message that indicates that the station has been successfully added
@@ -17,7 +16,7 @@
 #' @export
 
 
-add_stations <- function(stationname, lat, long, area=NULL, timezone=NULL, PIC=NULL,  username=NULL, password=NULL){
+add_stations <- function(stationname, lat, long, area=NULL, timezone=NULL, username=NULL, password=NULL){
 
   if(is.null(username)||is.null(password)){
     username = keyring::key_list("DMMongoDB")[1,2]
@@ -32,10 +31,9 @@ add_stations <- function(stationname, lat, long, area=NULL, timezone=NULL, PIC=N
 
     if(stationname %in% prop$stationname) {stop("Warning: Matching station detected")}
 
-    template <- prop[prop$stationname == "Tremere", ]
+    template <- prop[prop$stationname == "CQIRP", ]
 
     if(is.null(area)){area <- 100}
-    if(is.null(PIC)){PIC <- "xxxxxx"}
     if(is.null(timezone)){timezone <- "Australia/Brisbane"}
 
     #Input new station details into a template dataframe and insert into database --------
@@ -43,10 +41,8 @@ add_stations <- function(stationname, lat, long, area=NULL, timezone=NULL, PIC=N
     template$stationname<- stationname
     template$longitude<-long
     template$latitude<- lat
-    template$PIC <- PIC
     template$hectares <- area
     template$timezone <- timezone
-    template$shortname <- substr(stationname, 1, 3)
 
     rownames(template)<-c()
     rownames(template$geometry)<-c()
@@ -79,7 +75,7 @@ add_stations <- function(stationname, lat, long, area=NULL, timezone=NULL, PIC=N
     #Create a temporary paddock in the Paddocks collection using the polygon coordinates. This will be removed if other paddocks are added.
 
     propertyinfo <- get_stations(stationname, username = username, password = password,
-                                 fields = c("_id", "longitude", "latitude", "reports", "PIC", "timezone", "stationname"))
+                                 fields = c("_id", "longitude", "latitude", "reports", "timezone", "stationname"))
     propertyid <- propertyinfo$`_id`
 
     template <- paddocks$find(query = '{"stationname":"xxxxxx"}', fields = '{"_id":false}')
